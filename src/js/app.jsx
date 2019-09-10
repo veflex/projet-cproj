e6ef2c44d0f54987ab474adc0edd4c31d8cb21cb
veflex <alexandrelesimple75@gmail.com>
2019-09-10 13:47:42 +0200

    setting up react app

---
+++ src/js/app.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+// import api from './utils/api'
+
+import Home from './pages/home'
+
+export default props => {
+  return (
+    <div className="app-wrapper">
+      <h1>App wrapper</h1>
+      <Home />
+    </div>
+  )
+}
---
+++ src/js/pages/home.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+export default props => {
+  return (
+    <div className="app-wrapper">
+      <h1>HOOOOOOOME</h1>
+    </div>
+  )
+}
